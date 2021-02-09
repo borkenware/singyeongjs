73a0ad52298b9c59a90d204b1bc3b668ce988ad4
Cynthia <cynthia@cynthia.dev>
2021-02-09 18:46:25 +0100

    basic connect logic

--- src/client.ts
+++ src/client.ts
@@ -25,60 +25,127 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import type { CertMeta } from 'ws'
 import { URL } from 'url'
 import WebSocket from 'ws'
 import EventEmitter from './events.js'
 
-const DSN_REGEX = /^s?singyeong:\/\/.+(:.+)?@[\w-]+(:\d{1,5})?\/?(\?encoding=(json|etf|msgpack))?$/
-
 type ClientEvents = {
-  connected: () => void // todo
+  connected: (restricted: boolean) => void
   reconnected: () => void // todo
-  disconnected: () => void // todo
   message: () => void // todo
-  error: () => void // todo
+  close: () => void
+  error: (e: Error) => void
 }
 
-export type SingyeongClientOptions = {
-  clientId?: string
-  auth?: string
+export type SingyeongOpts = {
   reconnect?: boolean
-  tags?: string[]
+  checkServerIdentity?: (servername: string, cert: CertMeta) => boolean
 }
 
 export default class SingyeongClient extends EventEmitter<ClientEvents> {
   private readonly secure: boolean
-  private readonly baseUrl: URL
-  private isRestricted: boolean
-  private ws: WebSocket
+  private readonly dsn: URL
+  private readonly opts: SingyeongOpts
+  private encoding: 'json'
+  private ws?: WebSocket
+  private pings: number[]
 
   public readonly applicationId: string
   public readonly clientId: string
+  public readonly websocketUrl: URL
+  public isRestricted: boolean
 
-  get url () {
-    return this.baseUrl.href
+  get isConnected () {
+    return this.ws?.readyState === WebSocket.OPEN
   }
 
-  get restricted () {
-    return this.isRestricted
+  get isConnecting () {
+    return this.ws?.readyState === WebSocket.CONNECTING
   }
 
-  constructor (dsn: string, opts: SingyeongClientOptions) {
+  get ping () {
+    if (!this.isConnected) {
+      return 0
+    }
+
+    return this.pings.reduce((a, b) => a + b, 0) / this.pings.length
+  }
+
+  constructor (dsn: string, opts: SingyeongOpts = {}) {
     super()
 
-    if (!DSN_REGEX.test(dsn)) {
-      throw new URIError('The provided DSN is not a valid singyeong DSN.')
+    this.dsn = new URL(dsn)
+    if (this.dsn.protocol !== 'singyeong:' && this.dsn.protocol !== 'ssingyeong:') {
+      throw new URIError('Invalid DSN: The protocol must be singyeong or ssingyeong.')
+    }
+  
+    if (!this.dsn.username) {
+      throw new URIError('Invalid DSN: You mst specify a username. This represents your application ID on singyeong.')
     }
 
-    this.baseUrl = new URL(dsn)
-    this.secure = this.baseUrl.protocol === 'ssingyeong:'
+    this.secure = this.dsn.protocol === 'ssingyeong:'
+    this.applicationId = this.dsn.username
+    this.clientId = this.generateClientId() // todo: allow the user to set their own client id?
 
-    if (!opts.clientId) {
-      // todo: generate one
-    }
+    // todo: support msgpack encoding (and maybe etf?)
+    // - etf is non-restricted only, and performs very poorly on js (even using native bindings like Discord's erlpack).
+    // - unsure if it's worth the effort here (except maybe to reduce stress on singyeong because erlang is good at (de)serializing etf?)
+    this.encoding = 'json'
+    this.websocketUrl = new URL(`${this.secure ? 'wss' : 'ws'}://${this.dsn.hostname}:${this.dsn.port ?? 80}/gateway/websocket?encoding=${this.encoding}`)
+    this.opts = opts
+
+    this.isRestricted = false
+    this.pings = []
   }
 
-  async connect (): Promise<void> {} // todo
+  connect (): void {
+    if (this.isConnected || this.isConnecting) throw new Error('Already connected!')
+    this.createSocket()
+  }
 
-  disconnect (): void {} // todo
+  disconnect (): void {
+    if (!this.isConnected) throw new Error('Not connected!')
+    this.ws?.close()
+  }
+
+  private createSocket () {
+    this.pings = Array(5).fill(0)
+    this.ws = new WebSocket(this.websocketUrl, { checkServerIdentity: this.opts.checkServerIdentity })
+    this.ws.on('message', (msg) => this.handleMessage(msg))
+
+    this.ws.on('close', () => {
+      // todo: reconnect
+      // todo: don't emit if closure was expected (e.g. singyeong requested reconnect)
+      this.emit('close')
+    })
+    this.ws.on('error', (e) => this.emit('error', e))
+  }
+
+  private handleMessage (msg: WebSocket.Data) {
+    // todo: proper de-serialization based on the format
+    if (typeof msg !== 'string') {
+      this.emit('error', new Error('Received binary data, expected string'))
+      this.disconnect()
+      return
+    }
+
+    const payload = JSON.parse(msg)
+    this.pings.push(Date.now() - payload.ts)
+    this.pings.shift()
+
+    switch (payload.op) {
+      default:
+        this.emit('error', new Error(`Received an unexpected payload: unrecognized OP code ${payload.op}`))
+        this.disconnect()
+    }    
+  }
+
+  private generateClientId () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = Math.random() * 16 | 0
+      const v = c === 'x' ? r : (r & 0x3 | 0x8)
+      return v.toString(16)
+    })
+  }
 }
